2ae11ec94e74c6f054f5e803e346827c442f0d79
Corman <32941017+Cormanz@users.noreply.github.com>
2023-12-12 18:21:41 -0500

    Revert "Optimize move generation"

--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,4 @@
-use monster_chess::{board::{Board, game::{NORMAL_MODE, GameResults, MoveLegalResponse}, actions::{Move, SimpleMove, HistoryMove}, tests::get_time_ms}, games::chess::{pieces::{KING, PAWN, QUEEN, KNIGHT}, ATTACKS_MODE}, bitboard::BitBoard};
+use monster_chess::{board::{Board, game::{NORMAL_MODE, GameResults}, actions::{Move, SimpleMove}, tests::get_time_ms}, games::chess::{pieces::{KING, PAWN, QUEEN, KNIGHT}, ATTACKS_MODE}, bitboard::BitBoard};
 use rand::rngs::StdRng;
 
 use crate::evaluate::{evaluate, MATERIAL};
@@ -27,101 +27,26 @@ pub fn move_score<const T: usize>(
         },
         SimpleMove::Action(action) => {
             let opposing_team = board.state.teams[board.get_next_team(board.state.moving_team) as usize];
-
-            let mut score = 0;
     
             let dest = BitBoard::from_lsb(action.to);
-            if (dest & opposing_team).is_set() {
-                let mut captured_piece_type: usize = PAWN;
-                for piece_type in PAWN..KING {
-                    if (board.state.pieces[piece_type] & dest).is_set() {
-                        captured_piece_type = piece_type;
-                        break;
-                    }
+            if !(dest & opposing_team).is_set() {
+                return 0;
+            }
+
+            let mut captured_piece_type: usize = PAWN;
+            for piece_type in PAWN..KING {
+                if (board.state.pieces[piece_type] & dest).is_set() {
+                    captured_piece_type = piece_type;
+                    break;
                 }
-    
-                let moved = MATERIAL[&(action.piece_type as usize)];
-                let captured = MATERIAL[&captured_piece_type];            
-    
-                score += 1000 + (captured - moved);
             }
 
-            /*
-            // Promotion ordering: to be tested.
-            if action.info >= 1 && action.piece_type == PAWN as u16 {
-                let promotion_type = action.info - 1;
-                score += MATERIAL[&(promotion_type as usize)];
-            }
-            */
+            let moved = MATERIAL[&(action.piece_type as usize)];
+            let captured = MATERIAL[&captured_piece_type];            
 
-            score
+            1000 + (captured - moved)
         }
     }
-}
-
-fn quiescence<const T: usize>(
-    board: &mut Board<T>, 
-    search_info: &mut SearchInfo,
-    mut alpha: i32,
-    beta: i32
-) -> i32 {
-    let stand_pat = evaluate(board);
-    if stand_pat >= beta { return beta; }
-    if alpha < stand_pat { alpha = stand_pat; }
-
-    let moves = board.generate_moves(NORMAL_MODE);
-
-    let mut scored_moves = moves.into_iter().map(|action| {
-        let score = move_score(board, &action);
-        (action, score)
-    }).collect::<Vec<_>>();
-
-    scored_moves.sort_by(|&a, &b| {
-        b.1.cmp(&a.1)
-    });
-
-    let opposing_team = board.state.teams[board.get_next_team(board.state.moving_team) as usize];
-    for (action, _score) in scored_moves {
-        let dest = match action {
-            SimpleMove::Pass => continue,
-            SimpleMove::Action(action) => {
-                BitBoard::from_lsb(action.to)
-            }
-        };
-
-        if !(dest & opposing_team).is_set() { continue; }
-
-        let mut undo: Option<HistoryMove<T>> = None;
-        let MoveLegalResponse { is_legal, made_move } = board.game.controller.is_legal(board, &action, false);
-        if !is_legal {
-            if let Some(made_move) = made_move {
-                board.undo_move(made_move);
-                continue;
-            }
-        }
-
-        if let Some(made_move) = made_move {
-            undo = made_move;
-        }
-        
-        if undo.is_none() {
-            undo = board.make_move(&action);
-        }
-
-        search_info.nodes += 1;
-        let score = -quiescence(board, search_info, -beta, -alpha);
-        board.undo_move(undo);
-
-        if score > alpha {
-            alpha = score;
-        }
-
-        if alpha >= beta {
-            break; // Beta cutoff
-        }
-    }
-    
-    alpha
 }
 
 pub fn negamax<const T: usize>(
@@ -133,7 +58,7 @@ pub fn negamax<const T: usize>(
     beta: i32
 ) -> i32 {
     if depth == 0 { 
-        let eval = quiescence(board, search_info, alpha, beta);
+        let eval = evaluate(board);
         return eval;
     }
 
@@ -155,7 +80,19 @@ pub fn negamax<const T: usize>(
     let mut max = -1_000_000;
     let mut best_move: Option<Move> = None;
 
-    let moves = board.generate_moves(NORMAL_MODE);
+    let moves = board.generate_legal_moves(NORMAL_MODE);
+    
+    let resolution = board.game.resolution.resolve(board, &moves);
+
+    match resolution {
+        GameResults::Ongoing => {},
+        GameResults::Draw => {
+            return 0;
+        },
+        GameResults::Win(team) => {
+            return if team == board.state.moving_team { 100_000 - (ply as i32) } else { -100_000 + (ply as i32) };
+        }
+    }
 
     let mut scored_moves = moves.into_iter().map(|action| {
         let score = move_score(board, &action);
@@ -166,39 +103,19 @@ pub fn negamax<const T: usize>(
         b.1.cmp(&a.1)
     });
 
-    let mut legal_moves: Vec<Move> = vec![];
-
     for (action, _score) in scored_moves {
-        let mut undo: Option<HistoryMove<T>> = None;
-        let MoveLegalResponse { is_legal, made_move } = board.game.controller.is_legal(board, &action, false);
-        if !is_legal {
-            if let Some(made_move) = made_move {
-                board.undo_move(made_move);
-                continue;
-            }
-        }
-
-        if let Some(made_move) = made_move {
-            undo = made_move;
-        }
-        
-        if undo.is_none() {
-            undo = board.make_move(&action);
-        }
-
         search_info.nodes += 1;
+        let undo = board.make_move(&action);
         let score = -negamax(board, search_info, depth - 1, ply + 1, -beta, -alpha);
         board.undo_move(undo);
-        
-        legal_moves.push(action);
 
         if score > max {
             best_move = Some(action);
             max = score;
         }
 
-        if score > alpha {
-            alpha = score;
+        if max > alpha {
+            alpha = max;
         }
 
         if alpha >= beta {
@@ -208,18 +125,6 @@ pub fn negamax<const T: usize>(
 
     if ply == 0 && !search_info.ended {
         search_info.best_move = best_move;
-    }
-
-    let resolution = board.game.resolution.resolve(board, &legal_moves);
-
-    match resolution {
-        GameResults::Ongoing => {},
-        GameResults::Draw => {
-            return 0;
-        },
-        GameResults::Win(team) => {
-            return if team == board.state.moving_team { 100_000 - (ply as i32) } else { -100_000 + (ply as i32) };
-        }
     }
     
     return max;
