6dd2a1e315f2e6f0ffc06b5727640da25315b34a
Cormanz <mrccay12@gmail.com>
2023-12-12 14:48:31 -0500

    Implement MVV-LVA Ordering

--- src/search.rs
+++ src/search.rs
@@ -1,7 +1,7 @@
-use monster_chess::{board::{Board, game::{NORMAL_MODE, GameResults}, actions::Move, tests::get_time_ms}, games::chess::{pieces::KING, ATTACKS_MODE}};
+use monster_chess::{board::{Board, game::{NORMAL_MODE, GameResults}, actions::{Move, SimpleMove}, tests::get_time_ms}, games::chess::{pieces::{KING, PAWN, QUEEN, KNIGHT}, ATTACKS_MODE}, bitboard::BitBoard};
 use rand::rngs::StdRng;
 
-use crate::evaluate::evaluate;
+use crate::evaluate::{evaluate, MATERIAL};
 
 #[derive(Debug, Clone, Copy)]
 pub enum SearchEnd {
@@ -15,6 +15,38 @@ pub struct SearchInfo {
     pub nodes: u64,
     pub search_end: SearchEnd,  
     pub ended: bool
+}
+
+pub fn move_score<const T: usize>(
+    board: &mut Board<T>, 
+    action: &Move
+) -> i32 {
+    match action {
+        SimpleMove::Pass => {
+            0
+        },
+        SimpleMove::Action(action) => {
+            let opposing_team = board.state.teams[board.get_next_team(board.state.moving_team) as usize];
+    
+            let dest = BitBoard::from_lsb(action.to);
+            if !(dest & opposing_team).is_set() {
+                return 0;
+            }
+
+            let mut captured_piece_type: usize = PAWN;
+            for piece_type in PAWN..KING {
+                if (board.state.pieces[piece_type] & dest).is_set() {
+                    captured_piece_type = piece_type;
+                    break;
+                }
+            }
+
+            let moved = MATERIAL[&(action.piece_type as usize)];
+            let captured = MATERIAL[&captured_piece_type];            
+
+            1000 + (captured - moved)
+        }
+    }
 }
 
 pub fn negamax<const T: usize>(
@@ -62,8 +94,16 @@ pub fn negamax<const T: usize>(
         }
     }
 
-    for action in moves {
+    let mut scored_moves = moves.into_iter().map(|action| {
+        let score = move_score(board, &action);
+        (action, score)
+    }).collect::<Vec<_>>();
 
+    scored_moves.sort_by(|&a, &b| {
+        b.1.cmp(&a.1)
+    });
+
+    for (action, _score) in scored_moves {
         search_info.nodes += 1;
         let undo = board.make_move(&action);
         let score = -negamax(board, search_info, depth - 1, ply + 1, -beta, -alpha);
